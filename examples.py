8eb233e2d0588fe5798b389e79378a7ed161eadf
JesseLivezey <grndfthrprdx@gmail.com>
2013-01-18 17:11:07 -0800

    Created basic syntax example

---
+++ examples.py
@@ -0,0 +1,19 @@
+import urllib2
+
+#Some examples of data that can be grabbed from NextBusXMLFeed
+def main():
+    agencyList = urllib2.urlopen('http://webservices.nextbus.com/service/publicXMLFeed?command=agencyList')
+    for lines in agencyList:
+        print lines
+
+    routeList = urllib2.urlopen('http://webservices.nextbus.com/service/publicXMLFeed?command=routelist&a=actransit')
+    for lines in routeList:
+        print lines
+
+    routeConfig = urllib2.urlopen('http://webservices.nextbus.com/service/publicXMLFeed?command=routeConfig&a=actransit&r=18')
+    for lines in routeConfig:
+        print lines
+
+        
+if __name__ == '__main__':
+    main()
