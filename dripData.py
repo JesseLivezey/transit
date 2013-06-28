1e0be27695b19aaa088568382feba254f8708b8a
JesseLivezey <jesse.livezey@gmail.com>
2013-06-27 17:47:43 -0700

    finished drip loop, still untested

--- dripData.py
+++ dripData.py
@@ -1,4 +1,4 @@
-import sys, os, getopt, ConfigParser
+import sys, os, getopt, ConfigParser, time
 import sqlite3 as lite
 import dbaccess
 #Reads in config file that determines when type of data to take and how often
@@ -23,9 +23,19 @@ def main(argv):
     print 'Storing data in '+filename
     con = lite.connect(filename)
     with con:
+        table = 'predictions'
         cur = con.cursor()
-        cur.execute("CREATE TABLE StopPred(agency TEXT, route TEXT, stop TEXT, direction TEXT, time INT, prediction TEXT)")
-        prediction = predictions.Prediction('actransit','18','0305120')
+        cur.execute("CREATE TABLE IF NOT EXIST "+table+"(agency TEXT, route TEXT, stop TEXT, direction TEXT, qtime INT, epochTime INT, seconds INT, vehicle INT, tripTag INT, isDeparture TEXT, dirTag TEXT, minutes INT, block INT)")
+        while True:
+            time.sleep(10)
+            try:
+                preds = predictions.Prediction('actransit','18','0305120')
+                cur.execute("PRAGMA table_info("+table+")")
+                cols = cur.fetchall()
+                dbaccess.writePredictions(cur,table,preds.getPredictionsDB(cols))
+            except KeyboardInterupt:
+                break
+    print 'Done writing predictions to table: '+table+' in file: '+filename+'. Goodbye!'
 
     
 
